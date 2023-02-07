e25a98a09a29ea721ca003753708f16362ea71d4
Anton <anton.solovev.1990@mail.ru>
2023-02-07 23:31:02 +0700

    1 задание

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,15 +1,13 @@
-"use strict"
-const addTimer = () => {
-    const timer = document.getElementById("timer");
-    timer.textContent -= 1; {
-        if (timer.textContent === "0") {
-            alert("Вы победили в конкурсе!");
-            clearInterval(addTimer);
-        }
-    }   
-}
-
-setInterval(addTimer, 1000);
-
+"use strict";
+const timer = document.getElementById("timer");
+const intervalId = setInterval(() => {
+  timer.textContent -= 1;
+  {
+    if (timer.textContent === "0") {
+      alert("Вы победили в конкурсе!");
+      clearInterval(intervalId);
+    }
+  }
+}, 1000);
 
 
