274ac056e210cd0fecc491f955e4ebbf0130e951
Anton <anton.solovev.1990@mail.ru>
2023-02-08 22:28:00 +0700

    2 задание

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,1 +1,15 @@
+const cookie = document.getElementById("cookie");
 
+let clicker__counter = document.getElementById("clicker__counter");
+let count = 0;
+
+cookie.onclick = function () {
+  if (count % 2 === 0) {
+    cookie.width = 100;
+  } else {
+    cookie.width = 300;
+  }
+  
+  count++;
+  clicker__counter.textContent = count;
+};
