45a0a875d98a004d3d6a094f78d996cc2389b853
Anton <anton.solovev.1990@mail.ru>
2023-02-10 00:15:49 +0700

    3 задание

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,26 @@
+"use strict"
+
+const dead = document.getElementById("dead");
+const lost = document.getElementById("lost");
+
+for (let i = 0; i < 9; i++) {
+    const hole = getHole(i + 1);
+
+    hole.onclick = function () {
+        if (hole.className.includes("hole_has-mole") === true) {
+            dead.textContent++;
+        } else {
+            lost.textContent++;
+        }
+
+        if (dead.textContent === "10") {
+            alert("Победа!");
+            dead.textContent = 0;
+            lost.textContent = 0;
+        } else if (lost.textContent === "5") {
+            alert("Вы проиграли!");
+            dead.textContent = 0;
+            lost.textContent = 0;
+        }
+    }
+}
