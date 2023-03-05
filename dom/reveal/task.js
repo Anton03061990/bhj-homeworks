63429726bbd72f616b9d8ec6cf393aaf92de1b0b
Anton <anton.solovev.1990@mail.ru>
2023-03-05 15:06:22 +0700

    1 задание

---
+++ dom/reveal/task.js
@@ -0,0 +1,11 @@
+const div = Array.from(document.querySelectorAll(".reveal"));
+console.log(div);
+
+window.addEventListener("scroll", () => {
+  div.forEach((elem) => {
+    const { top, bottom } = elem.getBoundingClientRect();
+    if (top > 0 && bottom < window.innerHeight) {
+      elem.classList.add("reveal_active");
+    }
+  });
+});
