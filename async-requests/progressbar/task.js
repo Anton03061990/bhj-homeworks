864363f8a99ac957017006d0168bafb51c0ee75f
Anton <anton.solovev.1990@mail.ru>
2023-03-23 11:36:32 +0700

    3 задание

---
+++ async-requests/progressbar/task.js
@@ -0,0 +1,20 @@
+const progress = document.getElementById('progress');
+
+form.addEventListener('submit', check);
+
+function check(event) {
+
+    event.preventDefault();
+
+    const form = document.getElementById('form');
+    const formData = new FormData(form);
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', ' https://students.netoservices.ru/nestjs-backend/upload');
+    xhr.onprogress = function (event) {
+
+        progress.value = event.loaded / 1000000;
+    }
+    xhr.send(formData);
+
+}
